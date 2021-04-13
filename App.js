cdf51ee4cade0b20d14dcd3c357ea62668313381
Chetan Naik <chetannaik02.cn@gmail.com>
2021-04-13 19:22:13 +0530

    Displaying username on ProfileScreen

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
-import React from 'react';
-import Providers from './navigation/index'
-import * as firebase from 'firebase'
-import apiKeys from './config/keys';
-import AppLoading from 'expo-app-loading'
-import { useFonts } from 'expo-font';
+import React from "react"
+import Providers from "./navigation/index"
+import * as firebase from "firebase"
+import apiKeys from "./config/keys"
+import AppLoading from "expo-app-loading"
+import { useFonts } from "expo-font"
 
-
-
-
-
-if(firebase.apps.length===0){
-  firebase.initializeApp(apiKeys.firebaseConfig)
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(apiKeys.firebaseConfig)
 }
 
-
-
-
-
-
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        ubuntu: require("./assets/fonts/Ubuntu-Regular.ttf"),
+        "ubuntu-bold": require("./assets/fonts/Ubuntu-Bold.ttf"),
+    })
 
-  const [fontsLoaded] = useFonts({
-    'ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
-    'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
-  })
-
-  if (!fontsLoaded) {
-		return <AppLoading />;
-	} else {
-    return <Providers/>
-  }
-   
- 
+    if (!fontsLoaded) {
+        return <AppLoading />
+    } else {
+        return <Providers />
+    }
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,56 +1,54 @@
-import firebase from 'firebase'
-import React from 'react'
-import { View, Text ,StyleSheet} from 'react-native'
-import { useContext } from 'react/cjs/react.development'
-import FormButton from '../components/FormButton'
-import { AuthContext } from '../navigation/AuthProvider'
+import firebase from "firebase"
+import React, { useState } from "react"
+import { View, Text, StyleSheet } from "react-native"
+import { useContext } from "react/cjs/react.development"
+import FormButton from "../components/FormButton"
+import { AuthContext } from "../navigation/AuthProvider"
 
-import "firebase/firestore";
+import "firebase/firestore"
 
 export default function ProfileScreen() {
-
-
-        
-    const {user,logout} = useContext(AuthContext)
-
-    const fetchName = ()=>{
-
+    const { user, logout } = useContext(AuthContext)
+    const [name, setName] = useState("")
+    const fetchName = () => {
         // console.log("Name")
-        
-            firebase.firestore().collection("users").doc(user.uid).get().then((snapshot)=>{
-                if(snapshot.exists){
-                    console.log(snapshot.data())
-                }
-
-                else{
-                    console.log('does not exist')
+        firebase
+            .firestore()
+            .collection("users")
+            .doc(user.uid)
+            .get()
+            .then((snapshot) => {
+                if (snapshot.exists) {
+                    setName(snapshot.data().name)
+                    console.log(snapshot.data().name)
+                } else {
+                    console.log("does not exist")
                 }
             })
-        
-        
-    } 
-
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Welcome {user.uid}</Text>
-            <FormButton buttonTitle='fetchName' onPress={fetchName}/>
-            <FormButton buttonTitle='Logout' onPress={()=>logout()}/>
-           
+            <Text style={styles.text}>Welcome {name}</Text>
+            <FormButton buttonTitle="fetchName" onPress={fetchName} />
+            <FormButton buttonTitle="Logout" onPress={() => logout()} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#f9fafd',
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        padding:20
+    container: {
+        backgroundColor: "#f9fafd",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
     },
-    text:{
-        fontSize:20,
-        color:'#333333'
-    }
+    text: {
+        fontSize: 20,
+        color: "#333333",
+    },
 })

af5d59d8b3ad24802161b041772a07fc692135d2
Jayant Mukundam <jayantmukundam2811@gmail.com>
2021-04-13 17:08:45 +0530

    Remove keys.js from gitignore

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+
+export default{
+
+   firebaseConfig :{
+    apiKey: "AIzaSyB8iMU13F9gVp6XpSuZkJovEDru-odnH-Q",
+    authDomain: "cirl-a469e.firebaseapp.com",
+    projectId: "cirl-a469e",
+    storageBucket: "cirl-a469e.appspot.com",
+    messagingSenderId: "13474305420",
+    appId: "1:13474305420:web:1db6d9f332ab348d0c72a9",
+    measurementId: "G-9RT4NYYKTP"
+  }
+}
+
+
+  

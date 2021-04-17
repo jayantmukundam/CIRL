31002eafc1a78a31968b38f24fa4d9aadce43f69
Jayant Mukundam <jayantmukundam2811@gmail.com>
2021-04-17 12:03:38 +0530

    label screen styling_1

--- screens/LabelScreen.js
+++ screens/LabelScreen.js
@@ -6,24 +6,35 @@ import { windowHeight, windowWidth } from "../utils/Dimensions"
 
 const data=require('../assets/products.json')
 export default function LabelScreen() {
+
+    const margin=10
     return (
+
+        
         <View style={styles.container}>
 
             <View style={styles.title}>
-            <Text>ENERGY LABEL BELOW {}</Text> 
+                <Text>ENERGY LABEL BELOW {}</Text> 
             </View>
            
 
-                <Divider style={{backgroundColor:'black'}}/>
+            <Divider style={{backgroundColor:'black'}}/>
+
 
             <View style={styles.ratingPart}>
-              {/* <View> */}
-              <Image source={require('../assets/ratings.png')} style={{flex: 1,width:"70%",resizeMode: 'contain'}} />
-            {/* </View>   */}
+                    <View style={{height:"100%",width:2*windowWidth/3}}>
+                            <Image source={require('../assets/ratings.png')} style={{width:"100%",height:"100%",resizeMode:'contain'}} />
+                    </View>  
 
-            {/* <View style={styles.arrow}>
+                    <View style={{width:windowWidth/3,height:"100%"}}>
+                        <View style={{marginTop:60}}></View>
+                         <Image source={require('../assets/arrowB.png')} style={{width:"70%",resizeMode: 'contain'}} />
 
-            </View> */}
+                         {/* <Image source={require('../assets/arrowB.png')} style={{width:"70%",resizeMode: 'contain',marginTop:50}} /> */}
+                    
+                    </View> 
+                
+                    
           
             </View>
             
@@ -35,40 +46,49 @@ export default function LabelScreen() {
             <Divider style={{backgroundColor:'black'}} />
 
             <View style={styles.energyConsumption}>
-            <Text style={styles.energyconsText}>{data.details.energyCons}</Text>
-            <Image source={require('../assets/energycons.png')} style={{height:"190%",width:"30%",resizeMode:"contain"}}></Image>
+                <Text style={styles.energyconsText}>{data.details.energyCons}</Text>
+                <Image source={require('../assets/energycons.png')} style={{height:"190%",width:"30%",resizeMode:"contain"}}/>
             </View>
             
             <Divider style={{backgroundColor:'black'}} />
 
-            <View style={styles.first}>
+            <View style={{}}>
+
             
-            <View style={styles.ratedcap}>
-                <Image source={require('../assets/ratedcap.png')} style={{height: "35%",width:"60%",resizeMode:"contain"}}></Image>
-                <Text style={styles.ratedText}>{data.details.ratedCapacity}</Text>
-            </View> 
 
-            <View style={styles.duration}>
-                <Image source={require('../assets/duration.png')} style={{height:"35%",width:"60%",resizeMode:"contain"}}></Image>
-                <Text style={styles.durationText}>{data.details.programmeDurationHalf}</Text>
+                    <View style={styles.first}>
+                    
+                            <View style={styles.ratedcap}>
+                                <Image source={require('../assets/ratedcap.png')} style={{height: "70%",width:"50%",resizeMode:"contain"}}></Image>
+                                <Text style={styles.ratedText}>{data.details.ratedCapacity}</Text>
+                            </View> 
+
+                            <View style={styles.duration}>
+                                <Image source={require('../assets/duration.png')} style={{height:"60%",width:"50%",resizeMode:"contain"}}></Image>
+                                <Text style={styles.durationText}>{data.details.programmeDurationHalf}</Text>
+                            </View>
+
+                            <View style={styles.watercons}>
+                                <Image source={require('../assets/watercons.png')} style={{height:"60%",width:"50%",resizeMode:"contain"}}></Image>
+                                <Text style={styles.waterText}>{data.details.waterCons}</Text>
+                            </View>
+                    </View>
+
+                    <View style={styles.second}>
+                        <View style={styles.spin}>
+                        <Image source={require('../assets/spin.png')} style={{height:"80%",width:"50%",resizeMode:"contain",marginLeft:20}}></Image>
+                            <Text>Spin class '{data.details.spinClass}' </Text>
+                        </View>
+
+                        <View style={styles.noise}>
+                        <Image source={require('../assets/noise.png')} style={{height:"50%",width:"50%",resizeMode:"contain",marginLeft:20}}></Image>
+                            <Text>Airbone Noise Emissions {data.details.noise}</Text>
+                        </View>
+                    
+                    </View> 
+
             </View>
 
-            <View style={styles.watercons}>
-                <Image source={require('../assets/watercons.png')} style={{height:"35%",width:"60%",resizeMode:"contain"}}></Image>
-                <Text style={styles.waterText}>{data.details.waterCons}</Text>
-            </View>
-            </View>
-
-            <View style={styles.second}>
-            <View style={styles.spin}>
-                <Text>Spin class '{data.details.spinClass}' </Text>
-            </View>
-
-            <View style={styles.noise}>
-                 <Text>Airbone Noise Emissions {data.details.noise}</Text>
-            </View>
-            
-            </View> 
         </View>
     )
 }
@@ -96,11 +116,14 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         marginTop:15,
         marginBottom:15,
-        flexDirection:'row'
+        flexDirection:'row',
+        // backgroundColor:'black'
     },
     ratingPart:{
         height:windowHeight/2,
-        // flexDirection:'row'
+        //  backgroundColor:'blue',
+        
+         flexDirection:'row'
     },
     ratings:{
         // backgroundColor:'black',
@@ -109,15 +132,20 @@ const styles = StyleSheet.create({
     first:{
         flexDirection: 'row',
         margin:10,
+        // backgroundColor:'red',
+        height:windowHeight/8
+        
     },
     second:{
         flexDirection:'row',
         justifyContent:'center',
         alignItems:'center',
-        backgroundColor: 'black',
+        height:windowHeight/8,
+        //  backgroundColor: 'black',
     },
     ratedcap:{
         width:windowWidth/3,
+        
     },
     duration:{
         width:windowWidth/3,
@@ -137,11 +165,16 @@ const styles = StyleSheet.create({
     },
     ratedText:{
         fontSize:18,
+        marginLeft:10
     },
     durationText:{
         fontSize:18,
+        marginTop:10,
+        marginLeft:20
     },
     waterText:{
         fontSize:18,
+        marginTop:10,
+        marginLeft:20
     },
 })
